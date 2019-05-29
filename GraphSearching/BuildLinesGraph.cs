e60f5a6176f539fee2f39bdd1ab11d8e62dcd135
lesha K <alexei.jobs@gmail.com>
2019-05-29 19:29:00 +0300

    Master state.Ex 7 before changing

---
+++ GraphSearching/BuildLinesGraph.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphSearching {
+    class BuildLinesGraph {
+        public static Graph<int> BuildGraphLin(int SIZE) { // Squere size!
+            Graph<int> graph = new Graph<int>();
+            int GraphSIZE = SIZE*SIZE;
+            //int WIDTH = Convert.ToInt16(Math.Sqrt((double)GraphSIZE));
+            int WIDTH = SIZE;
+            int HEIGHT = WIDTH - 1;
+
+            for(int i = 1; i <= GraphSIZE; ++i) {
+                graph.AddNode(i);
+            }
+            //Console.WriteLine("Add nodes: " + graph.Count);
+
+            for (int i = 1; i <= GraphSIZE; ++i) {
+                //graph.AddNode(i);
+                if ((i % WIDTH) != 0) { // Not right edge!
+                    graph.AddEdge(i, i + 1, 1);
+                    //Console.WriteLine("-Add edge: " + i + ", " + (i + 1));
+                    if ((i / WIDTH) != HEIGHT) {
+                        graph.AddEdge(i, i + WIDTH, 1);
+                        //Console.WriteLine("-Add edge: " + i + ", " + (i + WIDTH));
+                    }
+                } else {
+                    if ((i / WIDTH) != WIDTH) {
+                        graph.AddEdge(i, i + WIDTH, 1);
+                        //Console.WriteLine("*Add edge: " + i + ", " + (i + WIDTH));
+                    }
+                }
+            }
+
+            //graph.AddEdge(1, 2, 1);
+            //graph.AddEdge(1, 6, 1);
+
+            return graph;
+        }
+
+        
+
+    }
+
+    
+}
---
+++ GraphSearching/PathNodeInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphSearching
+{
+    /// <summary>
+    /// Info about a node along a search path
+    /// </summary>
+    /// <typeparam name="T">type stored in graph node</typeparam>
+    class PathNodeInfo<T>
+    {
+        #region Fields
+
+        GraphNode<T> previous;
+        int cost;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates an info node with the given previous node
+        /// </summary>
+        /// <param name="previous">previous node in path</param>
+        public PathNodeInfo(GraphNode<T> previous, int cost)
+        {
+            this.previous = previous;
+            this.cost = cost;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the previous graph node in the path
+        /// </summary>
+        public GraphNode<T> Previous
+        {
+            get { return previous; }
+        }
+
+        public int Cost { get => cost; set => cost = value; }
+
+        #endregion
+    }
+}
